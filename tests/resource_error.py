b981b9cf2f4f970cabd53597db200a8935485bd6
Wichert Akkerman <wichert@wiggy.net>
2014-07-04 11:20:38 +0200

    Fix tests after route_path rename

--- tests/resource_error.py
+++ tests/resource_error.py
@@ -2,13 +2,13 @@ from rest_toolkit import resource
 from pyramid.httpexceptions import HTTPNotFound
 
 
-@resource(route_path='/keyerror')
+@resource('/keyerror')
 class KeyErrorResource(object):
     def __init__(self, request):
         raise KeyError('BOOM!')
 
 
-@resource(route_path='/http-error')
+@resource('/http-error')
 class HTTPErrorResource(object):
     def __init__(self, request):
         raise HTTPNotFound('BOOM!')
--- tests/resource_get_renderer.py
+++ tests/resource_get_renderer.py
@@ -1,7 +1,7 @@
 from rest_toolkit import resource
 
 
-@resource(route_path='/')
+@resource('/')
 class Resource(object):
     def __init__(self, request):
         pass
