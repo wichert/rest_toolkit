18609d2ae17e6d33d12204302bb213a6ea5cddc2
Wichert Akkerman <wichert@wiggy.net>
2014-11-04 11:08:49 +0100

    Onwards

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
-version = '0.6'
+version = '0.7'
 
 install_requires = [
         'pyramid',
